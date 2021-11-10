1de0219f5e51f2e567859cdafaacc35657d7f13e
sahana178 <85974914+sahana178@users.noreply.github.com>
2021-11-09 19:57:04 -0600

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,9 @@
+function get_score(){
+score = localStorage.getItem("score");
+  document.getElementById("update").innerHTML = "<h1> Score:" + score + "</h1>";
+}
+
+
+function back(){
+    window.location = "activity_1.html";
+}
